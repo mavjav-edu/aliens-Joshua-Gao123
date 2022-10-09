595b59d115baed2ecd5fa4f8b4e78b4ff5c19ef9
Joshua Gao <gamemaster565.jg@gmail.com>
2022-10-09 10:31:15 -0700

    making stars Co-authored-by: Mavaddat Javid <mavaddat@users.noreply.github.com>

--- star_grid/star.py
+++ star_grid/star.py
@@ -1,6 +1,7 @@
 import pygame
 from pygame.sprite import Sprite
 class Star(Sprite):
+    SCALE_FACTOR=0.05
     def __init__(self, ai_game):
         super().__init__()
         self.screen = ai_game.screen
@@ -10,6 +11,9 @@ class Star(Sprite):
             print("Error loading image")
             exit()
         self.rect = self.image.get_rect()
-        self.rect.x = self.rect.width
-        self.rect.y = self.rect.height
-        self.x = float(self.rect.x)
+        self.rect.x = self.SCALE_FACTOR*self.rect.width
+        self.rect.y = self.SCALE_FACTOR*self.rect.height
+        self.image = pygame.transform.scale(self.image,(self.rect.x,self.rect.y))
+        print(f"Image is '{self.image.get_rect()}' large")
+        self.x = 0
+        self.y = float(self.rect.y)
--- star_grid/star_grid.py
+++ star_grid/star_grid.py
@@ -7,20 +7,36 @@ class StarGrid:
         pygame.display.set_caption("Stars")
         self.stars = pygame.sprite.Group()        
         self._create_star()
+        
     def _update_screen(self):
         self.screen.fill((50, 50, 200))
         self.stars.draw(self.screen)
         pygame.display.flip()
+
     def run_game(self):
         while True:
             self._update_screen()
 
-    def _create_star(self):
+    def _create_stars(self):
         star = Star(self)
-        star_width, star_height = star.rect.size
-        star.x = star_width + 2 * star_width
-        star.rect.x = star.x
-        star.rect.y = star.rect.height + 2 * star.rect.height
+        star_width, star_height = star.rect.size if star.rect else (0,0)
+        available_space_x = self.settings.screen_width - (star_width)
+        number_stars_x = available_space_x // (2 * star_width)
+
+        available_space_y = self.settings.screen.height - (2 * star_height)
+        number_rows = available_space_y // (2 * star_height)
+
+        for row_number in range(number_rows):
+            for star_number in range(number_stars_x):
+                self._create_star(star_number, row_number)
+
+    def _create_star(self, star_number=0, row_number=0):
+        star = Star(self)
+        star_width, star_height = star.rect.size if star.rect is not None else (0,0)
+        star.x = star_width + 2 * star_width * star_number
+        if star.rect is not None:
+            star.rect.x = star.x
+            star.rect.y = star_height + 2 * star_height * row_number
         self.stars.add(star)
 
         
