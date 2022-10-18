429eeeb44a027ff6d0e9102bd51c9e2fb4049fc3
Mavaddat Javid <5055400+mavaddat@users.noreply.github.com>
2022-10-17 21:52:39 -0700

    get star to show up

--- star_grid/star.py
+++ star_grid/star.py
@@ -1,19 +1,26 @@
 import pygame
 from pygame.sprite import Sprite
+
+
 class Star(Sprite):
-    SCALE_FACTOR=0.05
+    SCALE_FACTOR = 0.15
+
     def __init__(self, ai_game):
         super().__init__()
         self.screen = ai_game.screen
-        self.image = pygame.image.load("star_grid/star-vector-png-transparent-image-pngpix-21.png")
+        self.image = pygame.image.load("star.png")
+
         # check if the image was loaded properly
-        if self.image == None:
+        if self.image is None:
             print("Error loading image")
             exit()
+        self.image = pygame.transform.scale(self.image,
+            (
+                self.SCALE_FACTOR * self.image.get_width(),
+                self.SCALE_FACTOR * self.image.get_height()
+            )
+        )
         self.rect = self.image.get_rect()
-        self.rect.x = self.SCALE_FACTOR*self.rect.width
-        self.rect.y = self.SCALE_FACTOR*self.rect.height
-        self.image = pygame.transform.scale(self.image,(self.rect.x,self.rect.y))
         print(f"Image is '{self.image.get_rect()}' large")
         self.x = 0
         self.y = float(self.rect.y)
--- star_grid/star_grid.py
+++ star_grid/star_grid.py
@@ -1,13 +1,15 @@
 import pygame
 from star import Star
+
+
 class StarGrid:
     def __init__(self):
         pygame.init()
         self.screen = pygame.display.set_mode((0, 0), pygame.FULLSCREEN)
         pygame.display.set_caption("Stars")
-        self.stars = pygame.sprite.Group()        
+        self.stars = pygame.sprite.Group()
         self._create_star()
-        
+
     def _update_screen(self):
         self.screen.fill((50, 50, 200))
         self.stars.draw(self.screen)
@@ -19,11 +21,11 @@ class StarGrid:
 
     def _create_stars(self):
         star = Star(self)
-        star_width, star_height = star.rect.size if star.rect else (0,0)
-        available_space_x = self.settings.screen_width - (star_width)
+        star_width, star_height = star.rect.size if star.rect else (0, 0)
+        available_space_x = self.screen.get_width() - (star_width)
         number_stars_x = available_space_x // (2 * star_width)
 
-        available_space_y = self.settings.screen.height - (2 * star_height)
+        available_space_y = self.screen.get_height() - (2 * star_height)
         number_rows = available_space_y // (2 * star_height)
 
         for row_number in range(number_rows):
@@ -32,14 +34,14 @@ class StarGrid:
 
     def _create_star(self, star_number=0, row_number=0):
         star = Star(self)
-        star_width, star_height = star.rect.size if star.rect is not None else (0,0)
+        star_width, star_height = star.rect.size if star.rect is not None else (0, 0)
         star.x = star_width + 2 * star_width * star_number
         if star.rect is not None:
             star.rect.x = star.x
             star.rect.y = star_height + 2 * star_height * row_number
         self.stars.add(star)
 
-        
+
 if __name__ == '__main__':
     ai = StarGrid()
     ai.run_game()
